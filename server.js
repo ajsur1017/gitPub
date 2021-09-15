87e2c1edf502b4e3fea9b7e92bac53683285ea46
Anthony <ajsur1017@gmail.com>
2021-09-14 22:30:47 -0400

    capitalize function working for name

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express();
 const port = 3000;
 const drinks = require("./models/drinks")
 
+function capitalize(drink) {
+    const newDrink = {...drink} 
+    newDrink.name = drink.name.charAt(0).toUpperCase() + drink.name.slice(1)
+    return newDrink
+};
+
 app.get('/', (req, res) => {
     res.send("Welcome to the Gitpub App!")
 });
 
 app.get('/drinks', (req, res) => {
-    res.render("drinks_index.ejs", {allDrinks: drinks});
+    res.render("drinks_index.ejs", {allDrinks: drinks.map(capitalize)})
 });
 
 app.listen(3000 , () => {
