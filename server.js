395b3bac983835e665617e321746b0d08d32afb5
Anthony <ajsur1017@gmail.com>
2021-09-14 21:25:35 -0400

    json data displayed

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const drinks = require("./models/drinks")
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Gitpub App!")
+});
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+});
+
+app.listen(3000 , () => {
+    console.log(`Listening on port ${port}`)
+});
